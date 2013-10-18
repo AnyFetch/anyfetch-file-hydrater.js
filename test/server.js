c2c011d8294ab8819f9c04e95325985e7fa05eed
Neamar <neamar@neamar.fr>
2013-10-18 16:05:44 +0200

    Callbacks

--- test/server.js
+++ test/server.js
@@ -21,8 +21,8 @@ describe('POST /hydrate', function() {
   it('should refuse request without file_path', function(done) {
     request(server).post('/hydrate')
       .send({
-        'metadatas': 'http://example.org/file',
-        'callback': 'http://localhost/'
+        'metadatas': {},
+        'callback': 'http://cluestr.com/callback'
       })
       .expect(405)
       .end(done);
@@ -32,7 +32,7 @@ describe('POST /hydrate', function() {
     request(server).post('/hydrate')
       .send({
         'metadatas': {},
-        'file_path': 'http://example.org/file'
+        'file_path': 'http://cluestr.com/file'
       })
       .expect(405)
       .end(done);
@@ -44,8 +44,8 @@ describe('POST /hydrate', function() {
       .post('/hydrate')
       .send({
         'metadatas': {},
-        'file_path': 'http://example.org/file',
-        'callback': 'http://localhost/'
+        'file_path': 'http://cluestr.com/file',
+        'callback': 'http://cluestr.com/callback'
       })
       .expect(204)
       .end(done);
@@ -56,8 +56,8 @@ describe('POST /hydrate', function() {
       .post('/hydrate')
       .send({
         'metadatas': {},
-        'file_path': 'http://example.org/file',
-        'callback': 'http://localhost/',
+        'file_path': 'http://cluestr.com/file',
+        'callback': 'http://cluestr.com/callback',
         'long_poll': true
       })
       .expect(200)
@@ -66,7 +66,6 @@ describe('POST /hydrate', function() {
           throw err;
         }
 
-        console.log(res.body);
         done();
       });
   });
