70caf3e0d7c176324580880b1f90b004722a0cee
Neamar <neamar@neamar.fr>
2013-12-05 11:31:23 +0100

    Test for long_poll and errors

--- test/errors.js
+++ test/errors.js
@@ -22,6 +22,7 @@ describe('Errors', function() {
   var hydrationServer = cluestrFileHydrater.createServer(config);
 
   it('should be handled gracefully', function(done) {
+    this.timeout(4000);
     request(hydrationServer).post('/hydrate')
       .send({
         file_path: 'http://cluestr.com/file',
@@ -33,6 +34,32 @@ describe('Errors', function() {
       .expect(204)
       .end(function() {});
 
-    hydrationServer.queue.drain = done;
+    hydrationServer.queue.drain = function(err) {
+      hydrationServer.queue.drain = null;
+      done(err);
+    };
+  });
+
+  it('should be handled gracefully with long_poll option', function(done) {
+    this.timeout(4000);
+    
+    request(hydrationServer).post('/hydrate')
+      .send({
+        file_path: 'http://cluestr.com/file',
+        callback: 'http://cluestr.com/result',
+        metadatas: {
+          "foo": "bar"
+        },
+        'long_poll': true
+      })
+      .expect(400)
+      .end(function(err, res) {
+        if(err) {
+          throw err;
+        }
+
+        res.body.should.have.property('message').and.include('ERR');
+        done();
+      });
   });
 });
