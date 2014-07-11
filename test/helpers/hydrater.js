28dcda255b6818dba023dd2ccfd7b1d8b2899fd6
Amoki <hugoduroux@gmail.com>
2014-07-11 11:09:19 +0200

    fix dates

--- test/helpers/hydrater.js
+++ test/helpers/hydrater.js
@@ -2,25 +2,27 @@
 
 require('should');
 
-var dummyHydrater = function(path, document, changes, cb) {
-  changes.metadata.hydrated = true;
 
-  cb(null, changes);
-};
-
-var config = {
-  hydrater_function: dummyHydrater,
-  logger: function(str, err) {
-    if(err) {
-      throw err;
-    }
-  }
-};
-
-var hydrate = require('../../lib/helpers/hydrater')(config.hydrater_function, config.logger);
 
 describe("Hydrated document", function() {
   it("should have only updated changes", function(done) {
+
+    var dummyHydrater = function(path, document, changes, cb) {
+      changes.metadata.hydrated = true;
+
+      cb(null, changes);
+    };
+
+    var config = {
+      hydrater_function: dummyHydrater,
+      logger: function(str, err) {
+        if(err) {
+          throw err;
+        }
+      }
+    };
+    var hydrate = require('../../lib/helpers/hydrater')(config.hydrater_function, config.logger);
+
     var task = {
       callback: "http://wedontcare.com",
       filepath: "/tmp/anyfetch-hydrater.test",
@@ -31,6 +33,36 @@ describe("Hydrated document", function() {
 
     hydrate(task, function(changes) {
       changes.should.have.keys(["metadata"]);
+      done();
+    });
+  });
+
+  it("should keep Dates", function(done) {
+    var dummyHydrater = function(path, document, changes, cb) {
+      changes.creation_date = new Date();
+
+      cb(null, changes);
+    };
+
+    var config = {
+      hydrater_function: dummyHydrater,
+      logger: function(str, err) {
+        if(err) {
+          throw err;
+        }
+      }
+    };
+    var hydrate = require('../../lib/helpers/hydrater')(config.hydrater_function, config.logger);
+    var task = {
+      callback: "http://wedontcare.com",
+      filepath: "/tmp/anyfetch-hydrater.test",
+      document: {
+        id: "azerty"
+      }
+    };
+
+    hydrate(task, function(changes) {
+      changes.should.have.keys(["creation_date"]);
       done();
     });
   });
