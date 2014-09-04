0df50e34709b38e1a6f8e71c1ad00d737a7711ca
Amoki <hugoduroux@gmail.com>
2014-09-04 14:56:29 +0200

    add test to check if childs arr well killed

--- test/helpers/hydrater.js
+++ test/helpers/hydrater.js
@@ -2,6 +2,9 @@
 
 require('should');
 var path = require("path");
+var async = require("async");
+var shellExec = require('child_process').exec;
+
 var createFakeApi = require('./fake-api.js');
 
 describe("hydrate()", function() {
@@ -94,5 +97,68 @@ describe("hydrate()", function() {
         done(err);
       });
     });
+  });
+});
+
+describe('Hydration', function() {
+
+  var fakeApi = createFakeApi();
+
+  fakeApi.patch('/result', function(req, res,next) {
+    res.send(204);
+    next();
+  });
+  before(function() {
+    fakeApi.listen(4243);
+  });
+
+  after(function() {
+    fakeApi.close();
+  });
+
+  it('should be cleaned every every time', function(done) {
+    this.timeout(10000);
+
+    var nodeProccessesAtStart;
+    async.waterfall([
+      function getActualNodeProccesses(cb) {
+        shellExec('ps aux | grep "[n]ode" -c', cb);
+      },
+      function setNodeProcessesNumber(stdout, stderr, cb) {
+        nodeProccessesAtStart = parseInt(stdout);
+        cb();
+      },
+      function hydrateManyTimes(cb) {
+        var config = {
+          hydrater_function: path.resolve(__dirname, '../hydraters/grep-hydrater.js'),
+          logger: function() {},
+        };
+        var hydrate = require('../../lib/helpers/hydrater.js')(config.hydrater_function, config.logger);
+
+        var task = {
+          file_path: "http://127.0.0.1:4243/afile",
+          callback: "http://127.0.0.1:4243/result",
+          document: {
+            id: "azerty"
+          },
+        };
+
+        var hydrationCount = 0;
+        async.whilst(
+          function test() {
+            hydrationCount += 1;
+            return hydrationCount < 10;
+          },
+          function hydrateAndCheck(cb) {
+            hydrate(task, function(err, changes) {
+              changes.metadata.nodeCount.should.eql(nodeProccessesAtStart + 1);
+              cb(err);
+            });
+          },
+          cb
+        );
+
+      }
+    ], done);
   });
 });
