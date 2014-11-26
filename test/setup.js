eb067575bc9e2affae796467cc839e332ad971d7
Neamar <neamar@neamar.fr>
2014-11-26 11:41:07 +0100

    Removed logging during tests

--- test/setup.js
+++ test/setup.js
@@ -6,3 +6,10 @@ before(function flushRedis(cb) {
   var client = redis.createClient();
   client.flushdb(cb);
 });
+
+
+before(function removeLogging() {
+  // Disable logging while testing
+  var log = require('../lib/index.js').log;
+  log.info = log.warn = log.error = function() {};
+});
